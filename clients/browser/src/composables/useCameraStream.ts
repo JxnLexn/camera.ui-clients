6f81a6d02676573747e0c6ecfc58632bcc974c5d
seydx <hi@seydx.dev>
2026-07-27 11:12:53 +0200

    remove background color from video element in useCameraStream

--- clients/browser/src/composables/useCameraStream.ts
+++ clients/browser/src/composables/useCameraStream.ts
@@ -217,7 +217,6 @@ export function useCameraStream(options: UseCameraStreamOptions): CameraStream {
     video.disablePictureInPicture = false;
     video.preload = 'auto';
     video.poster = 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAQAAAC1HAwCAAAAC0lEQVR42mNkYAAAAAYAAjCB0C8AAAAASUVORK5CYII=';
-    video.style.backgroundColor = '#000';
     video.style.position = 'absolute';
     video.style.width = '100%';
     video.style.height = '100%';
